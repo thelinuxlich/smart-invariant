47e4cd9607688cf68da6c7f789c0fca58d7ed9e2
Alisson <thelinuxlich@gmail.com>
2023-03-09 22:24:44 -0300

    It LIVES

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # smart_invariant
-Like invariant and tiny_invariant, but smarter
+
+Like invariant and tiny_invariant, but smarter.
+
+Differences from both
+
+- Although it's called smart_invariant, we suggest you import it as assert, because it's shorter and more readable
+- No prefix
+- No dependency on process.env.NODE_ENV, the message will be sent whether NODE_ENV is production or not
+- Message is required and must be a string
+- Accepts a optional third argument, which is a function that will be called if the invariant fails (be aware that this function won't be try-catched inside smart_invariant, you'd do it yourself in the function body). Example: Log to Sentry
+
+## Usage
+
+```ts
+import assert from 'smart_invariant';
+
+assert(1 === 1, '1 is not equal to 1'); // No error
+assert(1 === 2, '1 is not equal to 2', () => {
+  Sentry.captureMessage('1 is not equal to 2');
+}); // Error: 1 is not equal to 2 and Sentry will be notified
+```
---
+++ tests/index.ts
@@ -0,0 +1,49 @@
+import assert from "../src/index";
+import { expect, test, vi, assertType } from "vitest";
+test("should include a provided message when an invariant does throw", () => {
+	try {
+		assert(false, "my message");
+	} catch (e) {
+		expect(e).toBeInstanceOf(Error);
+		expect(e.message).toEqual("my message");
+	}
+});
+
+test("should not execute the optional function if the invariant does not throw", () => {
+	const optional_execution = vi.fn();
+	assert(true, "won't throw", optional_execution);
+	expect(optional_execution.mock.calls.length).toEqual(0);
+});
+
+test("should execute a message function if the invariant does throw", () => {
+	const message = vi.fn();
+	try {
+		assert(false, "will throw", () => message("foo"));
+	} catch (e) {
+		expect(e).toBeInstanceOf(Error);
+		expect(message.mock.calls.length).toBeGreaterThan(0);
+		expect(e.message).toEqual("will throw");
+	}
+});
+
+test("should correctly narrow a type (boolean)", () => {
+	try {
+		const value: boolean = false;
+		assert(value, "Value is false");
+		assertType<never>(value);
+	} catch {}
+});
+
+test("should correctly narrow a type (custom type)", () => {
+	type Nullable<T> = T | null;
+	type Person = { name: string };
+
+	function tryGetPerson(name: string): Nullable<Person> {
+		return { name };
+	}
+
+	const lula: Nullable<Person> = tryGetPerson("Lula");
+
+	assert(lula, "Alex is not null");
+	assertType<Person>(lula);
+});
